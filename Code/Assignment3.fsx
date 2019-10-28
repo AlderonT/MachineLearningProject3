e0fd69c10795b5e58ef978481aaac3eb30f45b6c
cmajor <chrismichelmajor@hotmail.com>
2019-10-28 16:18:44 -0600

    Added objects

--- Code/Assignment3.fsx
+++ Code/Assignment3.fsx
@@ -27,7 +27,16 @@ namespace Project3
 // OBJECTS
 //--------------------------------------------------------------------------------------------------------------
 
-        // Create a node object
+        // Create a Layer object to represent a layer within the neural network
+        type Layer = 
+            abstract member nodes           : float32[]                         // Sequence to make up vectors
+            abstract member nodeCount       : int                               // Number of nodes in the layer
+
+        // Create a ConnectionMatrix object to represent the connection matrix within the neural network
+        type ConnectionMatrix = 
+            abstract member weights         : float32[]                         // Sequence of weights within the matrix
+            abstract member inputLayer      : Layer                             // Input layer
+            abstract member outputLayer     : Layer                             // Output layer
 
 
 // FUNCTIONS
