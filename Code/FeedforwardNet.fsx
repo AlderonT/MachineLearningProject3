cc7c7eb756f77647d9627c23becb428ed6f39f77
chrism <chrismichelmajor@gmail.com>
2019-10-29 09:33:44 -0600

    Separated the FFNN and RBF into separate modules

--- Code/FeedforwardNet.fsx
+++ Code/FeedforwardNet.fsx
@@ -4,7 +4,7 @@
 //  Assignment #3, Fall 2019
 //  Chris Major, Farshina Nazrul-Shimim, Tysen Radovich, Allen Simpson
 //
-//  [DESCRIPTION]
+//  Module for a Feedforward Neural Network
 //
 //--------------------------------------------------------------------------------------------------------------
 
@@ -13,36 +13,28 @@
 namespace Project3
 
     // Load the file names from the same folder
-    #load "FeedforwardNet.fsx"
-    #load "RBFNet.fsx"
+    #load "ActivationFunction.fsx"
+    #load "DotProduct.fsx"
 
     // Open the modules
-    open FeedforwardNet
-    open RBFNet
+    open ActivationFunction
+    open DotProduct
 
     // Declare as a module
-    module Assignment3 = 
+    module FeedforwardNet = 
 
 
 // OBJECTS
 //--------------------------------------------------------------------------------------------------------------
 
-        // Create a Layer object to represent a layer within the neural network
-        type Layer = 
-            abstract member nodes           : float32[]                         // Sequence to make up vectors
-            abstract member nodeCount       : int                               // Number of nodes in the layer
-
-        // Create a ConnectionMatrix object to represent the connection matrix within the neural network
-        type ConnectionMatrix = 
-            abstract member weights         : float32[]                         // Sequence of weights within the matrix
-            abstract member inputLayer      : Layer                             // Input layer
-            abstract member outputLayer     : Layer                             // Output layer
 
 
 // FUNCTIONS
 //--------------------------------------------------------------------------------------------------------------
 
-        // Write out functions
+        // Function to run the Feedforward Network with Backpropogation
+        let FFNN inputs hiddenLayers hiddenNodes outputs = 
+            1                                                               // Write out functions
 
 
 // IMPLEMENTATIONS
--- Code/RBFNet.fsx
+++ Code/RBFNet.fsx
@@ -4,7 +4,7 @@
 //  Assignment #3, Fall 2019
 //  Chris Major, Farshina Nazrul-Shimim, Tysen Radovich, Allen Simpson
 //
-//  [DESCRIPTION]
+//  Module for a Radial Basis Function Neural Network
 //
 //--------------------------------------------------------------------------------------------------------------
 
@@ -13,36 +13,28 @@
 namespace Project3
 
     // Load the file names from the same folder
-    #load "FeedforwardNet.fsx"
-    #load "RBFNet.fsx"
+    #load "ActivationFunction.fsx"
+    #load "DotProduct.fsx"
 
     // Open the modules
-    open FeedforwardNet
-    open RBFNet
+    open ActivationFunction
+    open DotProduct
 
     // Declare as a module
-    module Assignment3 = 
+    module RBFNet = 
 
 
 // OBJECTS
 //--------------------------------------------------------------------------------------------------------------
 
-        // Create a Layer object to represent a layer within the neural network
-        type Layer = 
-            abstract member nodes           : float32[]                         // Sequence to make up vectors
-            abstract member nodeCount       : int                               // Number of nodes in the layer
-
-        // Create a ConnectionMatrix object to represent the connection matrix within the neural network
-        type ConnectionMatrix = 
-            abstract member weights         : float32[]                         // Sequence of weights within the matrix
-            abstract member inputLayer      : Layer                             // Input layer
-            abstract member outputLayer     : Layer                             // Output layer
 
 
 // FUNCTIONS
 //--------------------------------------------------------------------------------------------------------------
 
-        // Write out functions
+        // Function to run the Radial Basis Function Network
+        let RBF inputs gaussianBasisFn outputs addFFNN = 
+            1                                                               // Write out functions
 
 
 // IMPLEMENTATIONS
